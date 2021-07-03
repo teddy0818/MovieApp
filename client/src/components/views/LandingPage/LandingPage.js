5f7e982fd05350bda212074d5c7af3b68ffdf943
Byeongjin Hwang <taddy0818@naver.com>
2021-07-03 18:19:47 +0900

    [Client] API 에서 인기있는 영화 fetch 로 가져오기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { FaCode } from "react-icons/fa";
+import { API_URL, IMAGE_BASIC_URL, API_KEY} from "../../Config";
 
 function LandingPage() {
+
+    const [Movies, setMovies] = useState([])
+
+    useEffect(() => {
+        const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+        fetch(endPoint)
+            .then(response => response.json())
+            .then(response => {
+                setMovies([response.result])
+            })
+     
+    }, [])
+
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+      <div style={{ width: '100%', margin: '0' }}>
+          {/* { Main Image } */}
+
+          <div style={{ width:"85%", margin: '1rem auto' }}>
+              <h3>Movies by lastest </h3>
+              <hr />
+              {/* { Movie Grid Cards } */}
+
+          </div>
+
+          <div style={{ display: 'flex', justifyContent: 'center'}}>
+              <button>Load More</button>
+          </div>
+          
+      </div>
     )
 }
 
