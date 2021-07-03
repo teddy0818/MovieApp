29fd8bb30de15020145394934f0fc3523ec57638
Byeongjin Hwang <taddy0818@naver.com>
2021-07-03 19:26:55 +0900

    LandingPage  - 메인영화 이미지,제목,설명 불러오기 (제일 인기있는 영화 1개)

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from 'react'
 import { FaCode } from "react-icons/fa";
 import { API_URL, IMAGE_BASIC_URL, API_KEY} from "../../Config";
+import MainImage from "../LandingPage/Sections/MainImage"
 
 function LandingPage() {
 
     const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
 
     useEffect(() => {
         const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
@@ -12,7 +14,11 @@ function LandingPage() {
         fetch(endPoint)
             .then(response => response.json())
             .then(response => {
-                setMovies([response.result])
+
+                console.log(response)
+
+                setMovies([response.results])
+                setMainMovieImage(response.results[0])
             })
      
     }, [])
@@ -20,8 +26,15 @@ function LandingPage() {
 
     return (
       <div style={{ width: '100%', margin: '0' }}>
-          {/* { Main Image } */}
 
+          {/* { Main Image } */}
+          {/* MainMovieImage 가 있으면 M ainImage component를 가져와라 */}
+          {MainMovieImage &&
+            <MainImage image={`${IMAGE_BASIC_URL}w1280${MainMovieImage.backdrop_path}`}
+            title={ `${MainMovieImage.title}` }
+            description={ `${MainMovieImage.overview}` }
+            />
+          }
           <div style={{ width:"85%", margin: '1rem auto' }}>
               <h3>Movies by lastest </h3>
               <hr />
