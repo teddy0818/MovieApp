205c366bc9ef15293b8755265c9f9b11930ebb4d
Byeongjin Hwang <taddy0818@naver.com>
2021-07-04 15:50:27 +0900

    Favorite Button UI 생성

--- client/src/components/views/MovieDetail/MovieDetail.js
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -4,6 +4,7 @@ import MainImage from '../LandingPage/Sections/MainImage';
 import MovieInfo from './Sections/MovieInfo';
 import GridCards from '../commons/GridCards';
 import { Row } from "antd";
+import Favorite from './Sections/Favorite';
 
 function MovieDetail(props) {
 
@@ -54,6 +55,10 @@ function MovieDetail(props) {
 
             {/* Body */}
             <div style={{ width: '85%', margin: '1rem auto'}}>
+
+                <div style={{ display: 'flex', justifyContent: 'flex-end'}}>
+                    <Favorite />
+                </div>
                 
 
                 {/* Movie info */}
