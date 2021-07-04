1a69ed26daae32b21a4d1d5a9c75438047529834
Byeongjin Hwang <taddy0818@naver.com>
2021-07-04 19:26:52 +0900

    Favorite Page - 화면에 data 뿌리기

--- client/src/components/views/FavoritePage/FavoritePage.js
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -1,15 +1,17 @@
-import React,{useEffect} from 'react'
+import React,{useEffect, useState} from 'react'
 import Axios from 'axios'
 import './favorite.css'
 
 function FavoritePage() {
+
+    const [Favorties, setFavorties] = useState([])
 
     useEffect(() => {
         Axios.post('/api/favorite/getFavoriteMovie', {userFrom : localStorage.getItem('userId')})
         .then(response => {
             console.log(response.data)
             if(response.data.success) {
-                
+                setFavorties(response.data.favorites)
             } else {
                 alert('좋아요 한 영화들를 못가져왔습니다!!')
             }
@@ -28,11 +30,17 @@ function FavoritePage() {
                     <tr>
                         <th>Movie Title</th>
                         <th>Movie RunTime</th>
-                        <th>Remove from favorites</th>
+                        <th>Remove from favorsites</th>
                     </tr>
                 </thead>
                 <tbody>
-
+                    { Favorties.map((favorite, index) => (
+                        <tr key={index}>
+                            <td>{favorite.movieTitle}</td>
+                            <td>{favorite.movieRunTime}</td>
+                            <button>Remove</button>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
