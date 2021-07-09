9950d67d3d84c258fcf74d10a9c2b5860321d048
Byeongjin Hwang <taddy0818@naver.com>
2021-07-09 22:02:28 +0900

    영화 좋아요 시 좋아요 숫자 올리기, 좋아요 여부 판단

--- client/src/components/views/MovieDetail/Sections/LikeDisLikes.js
+++ client/src/components/views/MovieDetail/Sections/LikeDisLikes.js
@@ -2,10 +2,12 @@
 import React, {useEffect, useState} from 'react'
 import {Tooltip, Icon} from 'antd'
 import Axios from 'axios';
+import { useSelector } from 'react-redux'
 
 function LikeDisLikes(props) {
     
     const movieId = props.movieId
+    const userId = localStorage.getItem('userId')
     
     let variables = {
 
@@ -20,20 +22,52 @@ function LikeDisLikes(props) {
     }
 
     const [LikeNum, setLikeNum] = useState(0)
-
-    useEffect(() => {
-        //좋아요 숫자 가져오기
+    const [IsLike, setIsLike] = useState()
+    
+    const getLikeNum = () => {
+        //좋아요 수 가져오기
         Axios.post('/api/like/getLikeNum', {movieId})
         .then(response => {
-            console.log('likenum : ' + response.data.likesNum)
+            // console.log('likenum : ' + response.data.likesNum)
             if(response.data.success) {
                 setLikeNum(response.data.likesNum)
             } else {
                 alert('좋아요 수 가져오기 실패')
             }
         })
+    }
+    
+    const getIsLike = () => {
+        //내가 한 좋아요 가져오기 
+        Axios.post('/api/like/getMeLike', {movieId, userId})
+        .then(response => {
+            // console.log('likenum : ' + response.data.likesNum)
+            if(response.data.success) {
+                response.data.likesNum > 0 ? setIsLike(true) :  setIsLike(false)
+            } else {
+                alert('내가한 좋아요 여부 가져오기 실패')
+            }
+        })
+    }
 
+    useEffect(() => {
+        getLikeNum()
+        getIsLike()
     }, [])
+
+    const onClickLike = () => {
+        //좋아요 활성화
+        Axios.post('/api/like/saveLike', {movieId, userId})
+        .then(response => {
+            console.log('saveLike : ' + response.data)
+            if(response.data.success) {
+                getLikeNum()
+                getIsLike()
+            } else {
+                alert('좋아요 활성화 실패')
+            }
+        })
+    }
 
 
     return (
@@ -41,12 +75,12 @@ function LikeDisLikes(props) {
             <span key="comment-basic-like">
                 <Tooltip title="Like">
                     <Icon type="like"
-                          theme="filled"
-                           onClick=""
+                          theme={`${IsLike ? 'filled' : 'outlined'}`}
+                           onClick={onClickLike}
                     />
 
                 </Tooltip>
-                <span style={{ paddingLeft:'0px', cursor:'auto' }}> 1 </span>
+                <span style={{ paddingLeft:'0px', cursor:'auto' }}> {LikeNum} </span>
             </span>
 
             <span key="comment-basic-dislike">
@@ -57,7 +91,7 @@ function LikeDisLikes(props) {
                     />
 
                 </Tooltip>
-                <span style={{ paddingLeft:'0px', cursor:'auto' }}> 1 </span>
+                <span style={{ paddingLeft:'0px', cursor:'auto' }}>  </span>
             </span>
         </div>
     )
--- server/routes/like.js
+++ server/routes/like.js
@@ -9,13 +9,32 @@ const { Like } = require("../models/Like");
 
 router.post("/getLikeNum", (req, res) => {
     Like.find({ 'movieId' : req.body.movieId})
-        .populate('writer') 
         .exec((err, Likes) => {
             if(err) return res.status(400).send(err)
             res.status(200).json({ 
                 success: true, 
                 likesNum: Likes.length 
             })
+    })
+});
+
+router.post("/getMeLike", (req, res) => {
+    Like.find({ 'movieId' : req.body.movieId, 'userId': req.body.userId})
+        .populate('userId') 
+        .exec((err, Likes) => {
+            if(err) return res.status(400).send(err)
+            res.status(200).json({ 
+                success: true, 
+                likesNum: Likes.length 
+            })
+    })
+});
+
+router.post("/saveLike", (req, res) => {
+    const like = new Like(req.body)
+    like.save((err, doc) => {
+        if(err) return res.status(400).send(err)
+        return res.status(200).json({ success: true })
     })
 });
 
