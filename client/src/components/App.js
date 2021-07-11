2cbe88559ee3fc9f40e4faf20344f928ed360674
Byeongjin Hwang <taddy0818@naver.com>
2021-07-11 20:04:24 +0900

    title 변경

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { Route, Switch } from "react-router-dom";
 import Auth from "../hoc/auth";
 // pages for this product
@@ -14,13 +14,26 @@ import FavoritePage from './views/FavoritePage/FavoritePage'
 //true   only logged in user can go inside
 //false  logged in user can't go inside
 
+const useTitle = () => {
+  const [title, setTitle] = useState(null);
+
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerHTML = title;
+  };
+  useEffect(updateTitle, [title]);
+
+  return setTitle;
+};
+
 function App() {
+  const changeTitle = useTitle();
   return (
     <Suspense fallback={(<div>Loading...</div>)}>
       <NavBar />
       <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
         <Switch>
-          <Route exact path="/" component={Auth(LandingPage, null)} />
+          <Route exact path="/" component={Auth(LandingPage, null)}/>
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
           <Route exact path="/movie/:movieId" component={Auth(MovieDetail, null)} />
