41f1e7e669a9ca7b78882737612e8846d08ced41
Byeongjin Hwang <taddy0818@naver.com>
2021-07-12 23:29:20 +0900

    레이아웃 테스트

--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -9,7 +9,8 @@ function GridCards(props) {
             <Col lg={6} md={8} xs={24}>
                 <div style={{ position: 'relative' }}>
                     <a href={`/movie/${props.movieId}`} >
-                        <img style={{ width: '100%', height: '320px' }} src={props.image} alt={props.movieName} />
+                        <img style={{ width: '85%', height: '320px' }} src={props.image} alt={props.movieName} />
+                        <div style={{ color: 'black', fontSize: '15px'}}>{props.movieName}</div>
                     </a>
                 </div>
             </Col>
