0ea2b3d31f6bcea6f2979c298329dd790604125e
Byeongjin Hwang <taddy0818@naver.com>
2021-07-19 21:32:16 +0900

    주석 및 css 수정

--- client/src/components/views/LandingPage/Sections/MainImage.js
+++ client/src/components/views/LandingPage/Sections/MainImage.js
@@ -4,24 +4,24 @@ import React from 'react';
 // props 을 이용하여 이미지 링크를 받아옴
 function MainImage(props) {
     return (
-<div>
-        <div style={{
+<div style={{
             background: `linear-gradient(to bottom, rgba(0,0,0,0)
-            39%,rgba(0,0,0,0)
-            41%,rgba(0,0,0,0.65)
-            100%),
-            url('${props.image}'), #1c1c1c`,
-                height: '500px',
-                backgroundSize: '100%, cover',
-                backgroundPosition: 'center, center',
-                width: '100%',
-                position: 'relative'
-            }}>
+        39%,rgba(0,0,0,0)
+        41%,rgba(0,0,0,0.65)
+        100%),
+        url('${props.image}'), #1c1c1c`,
+            height: '650px',
+            backgroundSize: '100%, cover',
+            backgroundPosition: 'center, center',
+            width: '100%',
+            position: 'relative'
+        }}>
+        <div>
         <div style={{ position: 'absolute', maxWidth: '500px', bottom: '2rem', marginLeft: '2rem' }}>
             <h2 style={{ color: 'white' }}> {props.title} </h2>
             <p style={{ color: 'white', fontSize: '1rem'}}> {props.description} </p>
         </div>
-    </div>
+        </div>
 </div>
     )
 }
--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -7,15 +7,15 @@ function GridCards(props) {
         return (
             // 한 칼럼은 총 24. lg(최댓값):6, md(중간값):3, xs(최솟값):24
             <Col lg={6} md={8} xs={24}>
-                <div style={{ position: 'relative' }}>
+                <div style={{ position: 'relative', textAlign: 'center'}}>
                     <a href={`/movie/${props.movieId}`} >
                         <img style={{ width: '85%', height: '320px' }} src={props.image} alt={props.movieName} />
-                        <div style={{ color: 'black', fontSize: '15px'}}>{props.movieName}</div>
+                        <div style={{ color: 'black', fontSize: '15px', overflow:'hidden', textOverflow:'ellipsis', whiteSpace:'nowrap', fontWeight:'bolder', fontSize:'20px'}}>{props.movieName}</div>
                     </a>
                 </div>
             </Col>
         )  
-    } else {
+    } else { // 출연진들
         return (
             <Col lg={6} md={8} xs={24}>
                 <div style={{ position: 'relative' }}>
