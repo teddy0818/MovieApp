4d96b9e060f13e63e56f815fa48c50eaefa728e8
Byeongjin Hwang <taddy0818@naver.com>
2021-07-23 20:35:06 +0900

    제목 한글로 수정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -45,7 +45,7 @@ function LandingPage() {
         
           {MainMovieImage &&
             <MainImage image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
-            title={MainMovieImage.original_title}
+            title={MainMovieImage.title}
             description={MainMovieImage.overview}
             />
           }
