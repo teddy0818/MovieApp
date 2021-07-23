cd8e8425e4614344461bbb560f50e387a1178621
Byeongjin Hwang <taddy0818@naver.com>
2021-07-23 20:35:53 +0900

    디테일 페이지에서 배경사진 버그 수정

--- client/src/components/views/MovieDetail/MovieDetail.js
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -8,6 +8,7 @@ import Favorite from './Sections/Favorite';
 import Comment from './Sections/Comment';
 import Axios from 'axios';
 import LikeDisLikes from './Sections/LikeDisLikes'
+import { FaDAndD } from 'react-icons/fa';
 
 
 function MovieDetail(props) {
@@ -19,10 +20,9 @@ function MovieDetail(props) {
     const [Casts, setCasts] = useState([])
     const [ActorToggle, setActorToggle] = useState(false)
     const [Comments, setComments] = useState([])
+    const [MainIMG, setMainIMG] = useState(null)
 
     useEffect(() => {
-
-        // console.log(props.match);
 
         const endpointCrew = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
         
@@ -34,6 +34,14 @@ function MovieDetail(props) {
             .then(response => {
                 // console.log(response)
                 setMovie(response) 
+                setMainIMG(
+                    {
+                        backdrop_path: response.backdrop_path,
+                        title: response.title,
+                        overview : response.overview
+                    }
+                ) 
+                console.log(response)
             })
 
         fetch(endpointCrew)
@@ -45,7 +53,7 @@ function MovieDetail(props) {
 
         Axios.post('/api/comment/getComments', {movieId})
         .then(response => {
-            console.log(response.data)
+            // console.log(response.data)
             if(response.data.success) {
                 setComments(response.data.comments)
             } else {
@@ -69,10 +77,11 @@ function MovieDetail(props) {
             
             {/* Header */}
 
-            <MainImage image={`${IMAGE_BASE_URL}w1280${Movie.backdrop_path}`}
-            title={Movie.original_title}
-            description={Movie.overview}
-            />
+            {MainIMG && <MainImage
+            image={`${IMAGE_BASE_URL}w1280${MainIMG.backdrop_path}`}
+            title={MainIMG.title}
+            description={MainIMG.overview}
+            />}
 
             {/* Body */}
             <div style={{ width: '85%', margin: '1rem auto'}}>
