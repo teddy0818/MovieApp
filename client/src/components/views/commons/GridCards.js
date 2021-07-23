56821d7357214049e4e0b04750b139c4f9b533d8
Byeongjin Hwang <taddy0818@naver.com>
2021-07-23 23:08:18 +0900

    상세페이지 - 사진없는 배우 일 경우 특정이미지 띄우기

--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Col } from 'antd' // 한줄에 4개 뜨게 하려고
+import unKnown from '../../../image/unKnown.svg'
 
 function GridCards(props) {
 
@@ -18,8 +19,14 @@ function GridCards(props) {
     } else { // 출연진들
         return (
             <Col lg={6} md={8} xs={24}>
-                <div style={{ position: 'relative' }}>
-                        <img style={{ width: '100%', height: '320px' }} src={props.image} alt={props.characterName} />
+                <div style={{ position: 'relative', textAlign: 'center'}}>
+                    {props.image ?
+                    <img style={{ width: '100%', height: '320px' }} src={props.image} alt={props.characterName} />
+                    :
+                    <img style={{ width: '100%', height: '320px' }} src={unKnown} alt={props.characterName} />
+                }
+                    <div style={{ color: 'black', fontSize: '15px', overflow:'hidden', textOverflow:'ellipsis', whiteSpace:'nowrap', fontWeight:'bolder', fontSize:'20px'}}>{props.characterName}</div>
+                        
                 </div>
             </Col>
         )
