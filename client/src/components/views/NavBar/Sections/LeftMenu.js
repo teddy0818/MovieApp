e928c3d9ca03801065e2e60d6cba2ff259e7161a
Byeongjin Hwang <taddy0818@naver.com>
2021-07-23 22:05:17 +0900

    NavBar - 메뉴 수정

--- client/src/components/views/NavBar/Sections/LeftMenu.js
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -6,11 +6,11 @@ const MenuItemGroup = Menu.ItemGroup;
 function LeftMenu(props) {
   return (
     <Menu mode={props.mode}>
-    <Menu.Item key="mail">
+    {/* <Menu.Item key="mail">
       <a href="/">Home</a>
-    </Menu.Item>
+    </Menu.Item> */}
     <Menu.Item>
-      <a href="/favorite">Favorite</a>
+      <a href="/favorite">즐겨찾기</a>
     </Menu.Item>
   </Menu>
   )
