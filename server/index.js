8308f084a2d5f6373eeb305da32b0704aec87986
Byeongjin Hwang <taddy0818@naver.com>
2021-07-25 02:45:07 +0900

    make it better

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const mongoose = require("mongoose");
 const connect = mongoose.connect(config.mongoURI,
   {
     useNewUrlParser: true, useUnifiedTopology: true,
-    useCreateIndex: true, useFindAndModify: false
+    useCreateIndex: true, useFindAndModify: false, useMongoClient: true
   })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
